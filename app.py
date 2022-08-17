38af9104d98b97b7d24bafa0af4424a19c9d0475
Derek Jenkins <derektjenkins@gmail.com>
2022-08-16 23:40:29 -0400

    adds simple menu

--- app.py
+++ app.py
@@ -2,15 +2,12 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def say_hi():
-    return render_template('the-python-book.html')
+def show_home():
+    return render_template('home.html')
 
 @app.route('/contacts')
 def show_contacts():
-    contact_list = '<h1>Contact list:</h1>'
-    contacts = '<h3>github: https://github.com/quartzinquartz</h3>'
-    return contact_list + '\n' + contacts
+    return render_template('contacts.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
-
